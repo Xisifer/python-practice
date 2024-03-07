c3481ab0b93e667c0871c683de0c64c6f726c0a6
Scott Finlay <sfinlay@ibm.com>
2024-03-07 17:24:33 -0600

    Added proper parental logic with help from co-worker.
    TODO: Add handling for half-races (if player is Half-Orc, one parent must be Orc; etc)

--- char_creator_oop.py
+++ char_creator_oop.py
@@ -11,7 +11,7 @@ from dnd.origin_savage import SavageOrigins, SavageFlavor, SavageHomes
 from dnd.parents_status import ParentsStatus
 from dnd.parent_class import Parent
 from dnd.jobs_menu import PlayerJob, NPCJob, AllyJob
-
+from dnd.gender import Gender
 
 def name_input():
     char_name = input("Character name: ")
@@ -46,63 +46,123 @@ def creator_question(question_text, attribute):
 
 player_char = Character()
 
-player_char.name = name_input()
+# player_char.name = name_input()
 
 player_char.race = creator_question('Select race: ', Race)
 
 player_char.bg_childhood = creator_question('Your origin: ', Childhood)
 
-match player_char.race:
-    case Race.HUMAN:
-        player_char.bg_homeland = creator_question('You hail from ', HumanHomes)
-        player_char.bg_origin = creator_question('You were raised in ', HumanOrigins)
-        player_char.bg_flavor = creator_question('As you were growing up, ', HumanFlavor)
+# match player_char.race:
+#     case Race.HUMAN:
+#         player_char.bg_homeland = creator_question('You hail from ', HumanHomes)
+#         player_char.bg_origin = creator_question('You were raised in ', HumanOrigins)
+#         player_char.bg_flavor = creator_question('As you were growing up, ', HumanFlavor)
 
 
-    case Race.ELF | Race.HALFLING | Race.GNOME:
-        player_char.bg_homeland = creator_question('You hail from ', FeyHomes)
-        player_char.bg_origin = creator_question('You were raised in ', FeyOrigins)
-        player_char.bg_flavor = creator_question('As you were growing up, ', FeyFlavor)
+#     case Race.ELF | Race.HALFLING | Race.GNOME:
+#         player_char.bg_homeland = creator_question('You hail from ', FeyHomes)
+#         player_char.bg_origin = creator_question('You were raised in ', FeyOrigins)
+#         player_char.bg_flavor = creator_question('As you were growing up, ', FeyFlavor)
     
-    case Race.DRAGONBORN | Race.ORC | Race.GOBLIN | Race.KOBOLD | Race.GNOLL:
-        player_char.bg_homeland = creator_question('You hail from ', SavageHomes)
-        player_char.bg_origin = creator_question('You were raised in ', SavageOrigins)
-        player_char.bg_flavor = creator_question('As you were growing up, ', SavageFlavor)
+#     case Race.DRAGONBORN | Race.ORC | Race.GOBLIN | Race.KOBOLD | Race.GNOLL:
+#         player_char.bg_homeland = creator_question('You hail from ', SavageHomes)
+#         player_char.bg_origin = creator_question('You were raised in ', SavageOrigins)
+#         player_char.bg_flavor = creator_question('As you were growing up, ', SavageFlavor)
 
-    case _:
-        print('ERROR')
+#     case _:
+#         print('ERROR')
 
 
-# Player family is composed of 0 - 2 parents and 0 - 3 siblings
-player_char.parent_count = random.randint(1,2)
-print(f'You were raised by {player_char.parent_count} parents.')
+# ===================================================
+# Parents and Family
+
+
+# Both parents, single parent, orphan?
+
+
+
+
+
+
 # Family fate
 
+# By default, every character has a birth mother
+pc_parent1 = Parent()
+pc_parent1.gender = Gender.FEMALE
+pc_parent1.race = player_char.race
+pc_parent1.job = NPCJob.random()
+# Second parent defaults to Father, but this can change
+pc_parent2 = Parent()
+pc_parent2.gender = Gender.MALE
+pc_parent2.job = NPCJob.random()
+
+
+
+pc_parent1 = random.choice([None, pc_parent1])
+pc_parent2 = random.choice([None, pc_parent2])
+
+
+
+
+if pc_parent1.gender == Gender.FEMALE:
+    mom_dad = 'mother'
+elif pc_parent1.gender == Gender.MALE:
+    mom_dad = 'father'
+else:
+    mom_dad = 'parent'
+
+
+    print(f'Your {mom_dad} is a {pc_parent1.race} {pc_parent1.job}.')
+
+
+
+    pc_parent1 = Parent()
+    pc_parent1.gender = Gender.FEMALE
+    pc_parent1.race = player_char.race
+
+
+
+
+
+
+
+
+print(f"You are a {player_char.race}.")
+
+
 match player_char.bg_childhood:
-    
-    case Childhood.BORN:
-        if player_char.parent_count == 1:
-            pc_mother = Parent(gender='Female', race=player_char.race, job=NPCJob.random())
-            print(f'Your mother is a {pc_parent.gender} {pc_parent.race} {pc_parent.job}.')
-        else:
-            pc_mother = Parent(gender='Female', race=player_char.race, job=NPCJob.random())
-            pc_parent = Parent(gender='Male', race=player_char.race, job=NPCJob.random())
 
 
     case Childhood.ADOPTED:
         print('You were adopted.')
+        pc_parent1 = Character()
+        pc_parent2 = random.choice([None, Character()])
 
-        pc_parent = Parent(gender='Male', race=Race.random(), job=NPCJob.random())
+        print(f'Your adoptive parent is a {pc_parent1.gender} {pc_parent1.race} {pc_parent1.job}.')
 
-        print(f'Your adoptive parent is a {pc_parent.gender} {pc_parent.race} {pc_parent.job}.')
+    case Childhood.ORPHAN:
+        print('You are an orphan.')
+            
+    case _:
+        num_parents = bool(pc_parent1) + bool(pc_parent2)
+        print("You were {} {} parent{}.".format(
+            "adopted by" if Childhood.ADOPTED else "born to",
+            num_parents,
+            "s" if num_parents > 1 else ""))
 
-        
+for parent in pc_parent1, pc_parent2:
+    if parent:
+        print("Your {} is a {}.".format(
+            "mother" if parent.gender == Gender.FEMALE else "father",
+            parent.race))
 
-player_char.parents = creator_question('Status of your parents: ', ParentsStatus)
+  
 
-match player_char.parents_status:
-    case ParentsStatus.ALIVE:
-        print('Lucky you!')
-        pass
-    case ParentsStatus.INCIDENT:
-        print('Something happened to your parents.')
+# player_char.parents = creator_question('Status of your parents: ', ParentsStatus)
+
+# match player_char.parents_status:
+#     case ParentsStatus.ALIVE:
+#         print('Lucky you!')
+#         pass
+#     case ParentsStatus.INCIDENT:
+#         print('Something happened to your parents.')
