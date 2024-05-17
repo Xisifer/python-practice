8d9f692f501ec591f7d207fa889d97e54b7c6130
Scott Finlay <sfinlay@ibm.com>
2024-05-17 16:20:18 -0500

    Started folding generate_parents() into Parent class. Refactoring towards more OOP.

--- dnd/childhood.py
+++ dnd/childhood.py
@@ -6,8 +6,8 @@ class Birth(WeightedEnum):
     ORPHAN = ('Orphan: You HAD parents, but they\'re dead now.', 10)
 
 class Childhood(WeightedEnum):
-    UPPERCLASS = ('Your parents belong to the upper class. They were wealthy and affluent, and you never lacked for opportunities in life. Money opens doors, and you had the means to do and become anything you wanted to.')
-    MIDDLECLASS = ('Your parents belong to the middle class. They were honest workers who tried to set you up for success in life. You never went hungry or cold at night, but you were taught to live modestly within your means, keeping your head down to get by in life.')
-    LOWERCLASS = ('Your parents belong to the lower class. They were hard-working people who tried to give you the opportunities they never had, but were not usually successful. Times were often hard, and sometimes you had to go hungry or search for a new bed for the night.')
+    UPPERCLASS = ('Your parents belong to the upper class. They were wealthy and affluent, and you never lacked for opportunities in life. \nMoney opens doors, and you had the means to do and become anything you wanted to.')
+    MIDDLECLASS = ('Your parents belong to the middle class. They were honest workers who tried to set you up for success in life. \nYou never went hungry or cold at night, but you were taught to live modestly within your means, keeping your head down to get by in life.')
+    LOWERCLASS = ('Your parents belong to the lower class. They were hard-working people who tried to give you the opportunities they never had, but were not usually successful. \nTimes were often hard, and sometimes you had to go hungry or search for a new bed for the night.')
     
 
--- dnd/parent_class.py
+++ dnd/parent_class.py
@@ -1,15 +1,86 @@
 from dnd.character_class import Character
 from dnd.race import Race
+from dnd.jobs_menu import PlayerJob, NPCJob, AllyJob
+from dnd.gender import Gender
+
+from dnd.childhood import Birth
+
 
 # Define the Parent class with a race property
 class Parent(Character):
-    def __init__(self, gender=None, race=None, job=None, **kwargs):
+    def __init__(self, gender=None, race=None, job=None, kin=None, **kwargs):
         super().__init__()
         self.gender = gender
-        self.race = race
-        self.job = job
+
+        # pc_childhood starts empty
+        pc_childhood = ''
+        print('Generating parents...')
+        # By default, every character has a birth mother
+
+        self.gender = Gender.FEMALE
+        if kin:
+            self.race = kin.race
+        else: 
+            self.race = Race.random()
+        self.job = NPCJob.random()
 
 
+
+        # Second parent defaults to Father, but this can change
+        pc_parent2 = Parent()
+        pc_parent2.gender = Gender.MALE
+        pc_parent2.race = player_char.race
+        pc_parent2.job = NPCJob.random()
+
+        num_parents = 2
+
+        player_char.bg_birth = Birth.random()
+        
+        match player_char.bg_birth:
+            case Birth.ORPHAN:
+                pc_childhood += 'You are an orphan.'
+                pc_parent2=None
+                pc_parent1=None
+            case Birth.BORN:
+                s = "s" if num_parents > 1 else ""
+                pc_childhood += f"As a {player_char.race}, you were born to {num_parents} {player_char.race} parent{s}.\n"
+                for parent in pc_parent1, pc_parent2:
+                    if parent:
+                        if parent.gender == Gender.FEMALE: 
+                            pc_childhood += "Your mother is a {}.\n".format(parent.job)
+                        elif parent.gender == Gender.MALE: 
+                            pc_childhood += "Your father is a {}.\n".format(parent.job)
+                        else:
+                            pc_childhood += "Your non-binary parent is a {}.\n".format(parent.job)
+            case Birth.ADOPTED:
+                pc_parent2 = random.choice([None, pc_parent2])
+                num_parents = bool(pc_parent1) + bool(pc_parent2)
+
+                for parent in pc_parent1, pc_parent2:
+                    if parent:
+                        parent.race = Race.random()
+                        parent.gender = Gender.random()
+
+                s = "s" if num_parents > 1 else ""
+                pc_childhood += "You are a {}, but you were adopted by {} parent{}.\n".format(player_char.race, num_parents, s)
+                for parent in pc_parent1, pc_parent2:
+                    if parent:
+                        if parent.gender == Gender.FEMALE: 
+                            pc_childhood += "Your mother is a {} {}.\n".format(parent.race,parent.job)
+                        elif parent.gender == Gender.MALE: 
+                            pc_childhood += "Your father is a {} {}.\n".format(parent.race,parent.job)
+                        else:
+                            pc_childhood += "Your parent is a non-binary {} {}.\n".format(parent.race,parent.job)
+            case _:
+                print ('This should never happen.')
+    
+        parents = []
+        for parent in pc_parent1, pc_parent2:
+            if parent:
+                parents.append(parent)
+        finished_childhood = pc_childhood
+        print(finished_childhood)
+        return finished_childhood, parents
 
 # Example usage:
 # Create a few Parent instances to demonstrate the randomness
